d837408c4de864c9bc5addabd7fdf6aa8459abc0
JuanCoiroORT <juancoiroelizalde@gmail.com>
2026-03-28 10:24:20 -0300

    fixed imports

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Request, status
 
-from .models import Base
-from .database import engine
-from .routers import auth, todos, admin, users
+from models import Base
+from database import engine
+from routers import auth, todos, admin, users
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 
