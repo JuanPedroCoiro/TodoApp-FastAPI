09813a371191e3faba9fba6c273d7ad1f910479c
JuanCoiroORT <juancoiroelizalde@gmail.com>
2026-03-28 11:11:18 -0300

    fix root redirect

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def test(request: Request):
-    return RedirectResponse(url="/todos/todo-page", status_code=status.HTTP_302_FOUND)
+    return RedirectResponse(url="/auth/login-page", status_code=status.HTTP_302_FOUND)
 
 @app.get("/healthy")
 def health_check():
